032b0d12fba071d5f28a9d7d361b72df8e02e7c8
Hanseul Jun <hanseuljun@gmail.com>
2022-11-15 01:00:09 -0800

    Remove /scripts.

--- scripts/configure-linux.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cmake ../libpng -DCMAKE_INSTALL_PREFIX="../1.6.38-ssrobins/x64-linux" -DZLIB_ROOT="$(dirname $(pwd))/zlib-binaries/1.2.11/x64-linux" -DPNG_SHARED=0 -DCMAKE_C_FLAGS=-fPIC 
