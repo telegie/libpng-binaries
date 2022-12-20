1cffdaef3fa788b11717c583cb4a28fe3629a3fc
hanseuljun <hanseuljun@gmail.com>
2022-12-20 14:32:03 -0800

    Add rebuild flag to build.py.

--- build.py
+++ build.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
+import argparse
 import platform
+import shutil
 import subprocess
 from pathlib import Path
 
 
-def build_zlib():
-	here = Path(__file__).parent.resolve()
-	subprocess.run(["python3", f"{here}/zlib-binaries/build.py"], check=True)
+def build_zlib(parser_args):
+    here = Path(__file__).parent.resolve()
+    cmd_args = ["python3", f"{here}/zlib-binaries/build.py"]
+    if parser_args.rebuild:
+        cmd_args += ["--rebuild"]
+    subprocess.run(cmd_args, check=True)
 
 
 def build_x64_windows_binaries():
@@ -27,16 +32,17 @@ def build_x64_windows_binaries():
 def build_arm64_mac_binaries():
     here = Path(__file__).parent.resolve()
     subprocess.run(["cmake",
-    			    "-S", f"{here}/libpng",
-    			    "-B", f"{here}/build/arm64-mac",
+                    "-S", f"{here}/libpng",
+                    "-B", f"{here}/build/arm64-mac",
+                    "-G", "Ninja",
                     "-D", "CMAKE_OSX_ARCHITECTURES=arm64",
-    			    "-D", f"CMAKE_INSTALL_PREFIX={here}/install/arm64-mac",
-    			    "-D", f"ZLIB_ROOT={here}/zlib-binaries/install/arm64-mac",
-    			    "-D", "PNG_SHARED=0",
-    			    "-D", "PNG_ARM_NEON=off"],
+                    "-D", f"CMAKE_INSTALL_PREFIX={here}/install/arm64-mac",
+                    "-D", f"ZLIB_ROOT={here}/zlib-binaries/install/arm64-mac",
+                    "-D", "PNG_SHARED=0",
+                    "-D", "PNG_ARM_NEON=off"],
                    check=True)
-    subprocess.run(["make", "-C", f"{here}/build/arm64-mac", "-j8"], check=True)
-    subprocess.run(["make", "-C", f"{here}/build/arm64-mac", "install"], check=True)
+    subprocess.run(["ninja"], cwd=f"{here}/build/arm64-mac", check=True)
+    subprocess.run(["ninja", "install"], cwd=f"{here}/build/arm64-mac", check=True)
 
 
 def build_x64_mac_binaries():
@@ -44,49 +50,52 @@ def build_x64_mac_binaries():
     subprocess.run(["cmake",
                     "-S", f"{here}/libpng",
                     "-B", f"{here}/build/x64-mac",
+                    "-G", "Ninja",
                     "-D", "CMAKE_OSX_ARCHITECTURES=x86_64",
                     "-D", f"CMAKE_INSTALL_PREFIX={here}/install/x64-mac",
                     "-D", f"ZLIB_ROOT={here}/zlib-binaries/install/x64-mac",
                     "-D", "PNG_SHARED=0",
                     "-D", "PNG_ARM_NEON=off"],
                    check=True)
-    subprocess.run(["make", "-C", f"{here}/build/x64-mac", "-j8"], check=True)
-    subprocess.run(["make", "-C", f"{here}/build/x64-mac", "install"], check=True)
+    subprocess.run(["ninja"], cwd=f"{here}/build/x64-mac", check=True)
+    subprocess.run(["ninja", "install"], cwd=f"{here}/build/x64-mac", check=True)
 
 
 def build_arm64_ios_binaries():
     here = Path(__file__).parent.resolve()
     subprocess.run(["cmake",
-    			    "-S", f"{here}/libpng",
-    			    "-B", f"{here}/build/arm64-ios",
+                    "-S", f"{here}/libpng",
+                    "-B", f"{here}/build/arm64-ios",
+                    "-G", "Ninja",
                     "-D", "CMAKE_OSX_ARCHITECTURES=arm64",
-    			    "-D", "CMAKE_SYSTEM_NAME=iOS",
+                    "-D", "CMAKE_SYSTEM_NAME=iOS",
                     "-D", "CMAKE_OSX_DEPLOYMENT_TARGET=14.0",
-    			    "-D", f"CMAKE_INSTALL_PREFIX={here}/install/arm64-ios",
-    			    "-D", "PNG_SHARED=0",
-    			    "-D", "PNG_ARM_NEON=on",
-    			    "-D", "CMAKE_SYSTEM_PROCESSOR=arm"],
+                    "-D", f"CMAKE_INSTALL_PREFIX={here}/install/arm64-ios",
+                    "-D", "PNG_SHARED=0",
+                    "-D", "PNG_ARM_NEON=on",
+                    "-D", "CMAKE_SYSTEM_PROCESSOR=arm"],
                    check=True)
-    subprocess.run(["make", "-C", f"{here}/build/arm64-ios", "-j8"], check=True)
-    subprocess.run(["make", "-C", f"{here}/build/arm64-ios", "install"], check=True)
+    subprocess.run(["ninja"], cwd=f"{here}/build/arm64-ios", check=True)
+    subprocess.run(["ninja", "install"], cwd=f"{here}/build/arm64-ios", check=True)
 
 
 def build_arm64_iphonesimulator_binaries():
     here = Path(__file__).parent.resolve()
     subprocess.run(["cmake",
-    			    "-S", f"{here}/libpng",
-    			    "-B", f"{here}/build/arm64-iphonesimulator",
+                    "-S", f"{here}/libpng",
+                    "-B", f"{here}/build/arm64-iphonesimulator",
+                    "-G", "Ninja",
                     "-D", "CMAKE_OSX_ARCHITECTURES=arm64",
-    			    "-D", "CMAKE_SYSTEM_NAME=iOS",
+                    "-D", "CMAKE_SYSTEM_NAME=iOS",
                     "-D", "CMAKE_OSX_DEPLOYMENT_TARGET=14.0",
-    			    "-D", f"CMAKE_INSTALL_PREFIX={here}/install/arm64-iphonesimulator",
-    			    "-D", "PNG_SHARED=0",
-    			    "-D", "PNG_ARM_NEON=on",
-    			    "-D", "CMAKE_SYSTEM_PROCESSOR=arm",
-    			    "-D", "CMAKE_OSX_SYSROOT=iphonesimulator"],
+                    "-D", f"CMAKE_INSTALL_PREFIX={here}/install/arm64-iphonesimulator",
+                    "-D", "PNG_SHARED=0",
+                    "-D", "PNG_ARM_NEON=on",
+                    "-D", "CMAKE_SYSTEM_PROCESSOR=arm",
+                    "-D", "CMAKE_OSX_SYSROOT=iphonesimulator"],
                    check=True)
-    subprocess.run(["make", "-C", f"{here}/build/arm64-iphonesimulator", "-j8"], check=True)
-    subprocess.run(["make", "-C", f"{here}/build/arm64-iphonesimulator", "install"], check=True)
+    subprocess.run(["ninja"], cwd=f"{here}/build/arm64-iphonesimulator", check=True)
+    subprocess.run(["ninja", "install"], cwd=f"{here}/build/arm64-iphonesimulator", check=True)
 
 
 def build_x64_linux_binaries():
@@ -94,17 +103,26 @@ def build_x64_linux_binaries():
     subprocess.run(["cmake",
                     "-S", f"{here}/libpng",
                     "-B", f"{here}/build/x64-linux",
+                    "-G", "Ninja",
                     "-D", f"CMAKE_INSTALL_PREFIX={here}/install/x64-linux",
                     "-D", f"ZLIB_ROOT={here}/zlib-binaries/install/x64-linux",
                     "-D", "PNG_SHARED=0",
                     "-D", "CMAKE_C_FLAGS=-fPIC"],
                    check=True)
-    subprocess.run(["make", "-C", f"{here}/build/x64-linux", "-j8"], check=True)
-    subprocess.run(["make", "-C", f"{here}/build/x64-linux", "install"], check=True)
+    subprocess.run(["ninja"], cwd=f"{here}/build/x64-linux", check=True)
+    subprocess.run(["ninja", "install"], cwd=f"{here}/build/x64-linux", check=True)
 
 
 def main():
-    build_zlib()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rebuild", action="store_true")
+    parser_args = parser.parse_args()
+
+    build_zlib(parser_args)
+
+    here = Path(__file__).parent.resolve()
+    if parser_args.rebuild:
+        shutil.rmtree(f"{here}/build")
 
     if platform.system() == "Windows":
         build_x64_windows_binaries()
